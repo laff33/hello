e6ced7b760e70aeb4f8c9cdd1edec6fffa068745
laff33 <sublist@pm.me>
2021-04-01 21:03:31 -0400

    additional lines

--- playground.py
+++ playground.py
@@ -1,2 +1,13 @@
 name = input("Name: ")
 print(f"Hello, {name}")
+
+n = int(input("Number: "))
+if n > 0:
+    print(f"${n}")
+elif n < 0:
+    print (f"{n} is negative")
+else:
+    print("n is zero")
+
+langs = (jp, fr, de, it)
+print(f"We translate {len(langs)} languages.")
