5e0cc66b9fa161a7800e07a21e2d875c1405844d
laff33 <sublist@pm.me>
2021-04-01 21:12:30 -0400

    new lines

--- playground.py
+++ playground.py
@@ -1,13 +1,20 @@
-name = input("Name: ")
-print(f"Hello, {name}")
+#name = input("Name: ")
+#print(f"Hello, {name}")
 
-n = int(input("Number: "))
+'''n = int(input("Number: "))
 if n > 0:
     print(f"${n}")
 elif n < 0:
     print (f"{n} is negative")
 else:
-    print("n is zero")
+    print("n is zero")'''
 
-langs = (jp, fr, de, it)
+langs = ("jp", "fr", "de", "it", "es")
 print(f"We translate {len(langs)} languages.")
+
+for x in range(6):
+    print(x)
+
+houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+houses["Hermione"] = "Gryffindor"
+print(houses["Harry"])
